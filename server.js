e7b2ab2f07051c811aec67ddeb6f67650402ec0b
Joshua <joshuahawi24@gmail.com>
2024-12-11 16:33:01 +0300

    Updated server

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const bcrypt = require('bcrypt');
 bcrypt.hash(myPlaintextPassword, 13, (err, hash) => {
     console.log(hash);
     //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
-    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    bcrypt.compare(someOtherPlaintextPassword, hash, (err, res) => {
       console.log(res); //true
     });
   });
