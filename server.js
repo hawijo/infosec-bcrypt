10d77200577d946655cb00aebd741bbf39b52d46
Joshua <joshuahawi24@gmail.com>
2024-12-16 15:22:18 +0300

    Updated server

--- server.js
+++ server.js
@@ -11,37 +11,16 @@ const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 const bcrypt = require('bcrypt'); 
-
-bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log('Hash:', hash);
-  
-  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log('Do passwords match?', res);
-  });
-});
-
+// For this challenge, you don't need any async logic here, so remove it.
 //END_ASYNC
 
 
 //START_SYNC
+const hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log('Hash:', hash);
 
-// Typically, you would put bcrypt.hashSync() or bcrypt.compareSync() here if asked.
-// No changes required here for the async challenge. 
-let hash = bcrypt.hashSync(myPlaintextPassword, saltRounds)
-
-console.log(hash);
-  
-let compare =   bcrypt.compareSync(myPlaintextPassword, hash)
-console.log('Do passwords match?', compare);
-
+const result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log('Do passwords match?', result);
 //END_SYNC
 
 
